63b74c06f161de134834b53ddf0c920aaa4dd5e7
OwagaCJ <cedricjoe56@gmail.com>
2022-03-14 02:12:25 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase letters
+ * use the putchar function
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
