cef1d3b0ade186e8fef001268474ec1bc810095f
OwagaCJ <cedricjoe56@gmail.com>
2022-03-14 11:27:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base 10
+ * use the putchar funtion only
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit combinations
+ * use the putchar function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+	int n;
+
+	for (i = n - 1; i > 0; i--)
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
