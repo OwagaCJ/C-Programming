3de7a5441f833627d96d1b58f4b7953caaa2c894
OwagaCJ <cedricjoe56@gmail.com>
2022-03-14 14:59:47 -0700

    Unix is basically a simple operating system.1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
 /**
  * main - program that prints statement to the standard error
  * do not use printf or puts
- * Return: 0
+ * Return: 0 (Success)
  */
 
 int main(void)
 
 {
-write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
