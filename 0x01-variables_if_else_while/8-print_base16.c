8452a85cc4a9790400fe69a828e685836820fc28
OwagaCJ <cedricjoe56@gmail.com>
2022-03-14 16:00:38 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print all digits of base sixteen
+ * use only the putchar function
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+	int i;
+	char c;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+return (0);
+}
