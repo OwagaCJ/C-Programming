3a86c792f2da1d15fc8135a2db836fdcd397f5eb
OwagaCJ <cedricjoe56@gmail.com>
2022-03-18 19:17:42 -0700

    all included

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print digits
+ * use _putchar twice
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ * use only _putchar
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; 1++)
+	{
+		for (j = 0; j <= 14, j++)
+		_putchar((c / 10) + '0');
+		_putchar((c % 10) + '0');
+	}
+		_putchar('\n');
+}
