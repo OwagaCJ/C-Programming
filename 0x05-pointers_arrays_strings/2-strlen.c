7fa24e60a78964e532fd7bdc0c60ec7b98a7eaef
OwagaCJ <cedricjoe56@gmail.com>
2022-03-21 17:55:09 -0700

    give length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strlen - return the length of a string
+ * @s: length of the string
+ * Return: length of the string s
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
