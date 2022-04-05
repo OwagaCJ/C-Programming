7de5aca0af95d73b6adf769e7115c3a8541e321a
OwagaCJ <cedricjoe56@gmail.com>
2022-04-05 07:18:02 -0700

    constant byte

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: the destination
+ * @src: the source
+ * @n: bytes from the momory
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[p] = src[p];
+	}
+
+	return (dest);
+
+}
