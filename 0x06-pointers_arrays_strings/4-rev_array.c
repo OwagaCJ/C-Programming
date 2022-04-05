ecdc0f0b871e79d5c34683e561d88423a227e1d2
OwagaCJ <cedricjoe56@gmail.com>
2022-04-05 06:31:12 -0700

    concartnate

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,16 +1,19 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * reverse_array - reverses content of an array
  * @a: parameter 1
  * @n: number of elements
- * Return: 1
+ * Return: 0
  */
 
 void reverse_array(int *a, int n)
 {
 	int ch;
 	int i;
+
+	n = strlen(*a);
 
 	for (i = 0; i < n / 2; i++)
 	{
