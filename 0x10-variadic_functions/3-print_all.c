10d4d7babb4a602f44d1f4943545d8a0e58d374e
OwagaCJ <cedricjoe56@gmail.com>
2022-04-14 16:40:46 -0700

    print string

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anything
+ * @formart: list of types of arguments
+ *
+ * Return: none
+ */
+
+void print_all(const char * const format, ...)
+
+{
+	int i =0;
+
+	char *str, 
+}
